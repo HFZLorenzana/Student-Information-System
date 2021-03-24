b9f9b0317b6af1a88d3843fc13ae87c346b5bd40
ZorenX <72563196+ZoreniX@users.noreply.github.com>
2021-03-25 01:48:08 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student.information.system;
+
+/**
+ *
+ * @author Zoren X
+ */
+public class Student {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String idNumber;
+    private int birthYear;
+    private String course;
+    private String year;
+
+    public Student() {
+        this.firstName = "";
+        this.middleName = "";
+        this.lastName = "";
+        this.idNumber = "";
+        this.birthYear = 0;
+        this.course = "";
+        this.year = "";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    
+}
